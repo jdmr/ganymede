3249783200a60354833fa346dc1fa7dad3252e35
J. David Mendoza <jdmendoza@swau.edu>
2023-05-12 08:28:59 -0500

    Initial Commit

---
+++ ganymede/ganymedeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ganymedeApp.swift
+//  ganymede
+//
+//  Created by David Mendoza on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ganymedeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
